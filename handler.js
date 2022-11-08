f3c37506bd0746c56666917625d4f4bce5b57599
Neyzz21 <107815428+Neyzz21@users.noreply.github.com>
2022-11-08 22:56:34 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -47,7 +47,7 @@ module.exports = {
         case 'imageMessage':
         case 'videoMessage':
         case 'audioMessage':
-          if (!m.key.fromMe) return
+          if (!m.key.fromMe) true
           if (!m.msg.url) await this.updateMediaMessage(m)
           break
       }
